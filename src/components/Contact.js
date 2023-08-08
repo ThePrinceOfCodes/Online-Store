721752367b1f358ccce8a9affba57d2586928cfd
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-08 17:52:07 +0100

    contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,22 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return <Wrapper>
+    <div className='section-center'>
+      <h3>Join our newsletter and get amazing discounts</h3>
+      <div className='content'>
+        <p>A process VM, sometimes called an application virtual machine,
+          or Managed Runtime Environment (MRE), runs as a normal application
+          inside a host OS and supports a single process
+        </p>
+        <form className='contact-form'>
+          <input type='email' className='form-input' placeholder='enter email' />
+          <button type='submit' className='submit-btn'>subscribe</button>
+        </form>
+      </div>
+     
+    </div>
+  </Wrapper>
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
