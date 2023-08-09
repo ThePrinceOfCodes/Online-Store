05c35d4971882e0a5c23018e8c2dc54ab4e74fa2
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-09 13:52:51 +0100

    companies categories ui

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -31,6 +31,14 @@ const Filters = () => {
             })}
           </div>
         </div>
+        <div className='form-control'>
+          <h5>Company</h5>
+          <select name='company' value={company} onChange={updateFilters} className='company'>
+            {companies.map((c, index) => {
+              return <option key={index} value={c}>{ c }</option>
+            })}
+          </select>
+        </div>
       </form>
     </div>
   </Wrapper>
