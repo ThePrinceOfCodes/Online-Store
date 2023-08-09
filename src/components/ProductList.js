48c803c1444a16b9db52e1c01db8fe04320c6ffb
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-09 10:38:31 +0100

    products list view

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,18 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 const ProductList = () => {
-  const { filtered_products: products } = useFilterContext();
+  const { filtered_products: products , list_view, grid_view} = useFilterContext();
+
+  if (products.length < 1) {
+    return <h5 style={{ textTransform: 'none'}}>
+      sorry no products match your search...
+    </h5>
+  }
+
+  if (!grid_view) {
+    return <ListView products = {products} />
+  }
+
   return <GridView products = {products} />
 }
 
