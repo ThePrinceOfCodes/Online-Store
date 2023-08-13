123c782ff74a234e0f63415155531765eab01dc8
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-13 01:25:38 +0100

    calculate cart totals

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -53,6 +53,7 @@ export const CartProvider = ({ children }) => {
 
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(state.cart))
+    dispatch({type: COUNT_CART_TOTALS})
   },[state.cart]
   )
 
--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -78,6 +78,20 @@ const cart_reducer = (state, action) => {
     return{...state, cart:tempCart}
   }
 
+  if (action.type === COUNT_CART_TOTALS) {
+    const {total_amount, total_items } = state.cart.reduce((total, cartItem) => {
+      const { amount, price } = cartItem
+      total.total_items += amount
+      total.total_amount += price * amount
+      return total
+    }, {
+      total_items: 0, total_amount: 0
+    })
+
+    return { ...state, total_amount, total_items }
+    
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
