b59fecb7e658e9246ab10aab5a8e872322bfe0d2
Ezenwobodo Chukwuebuka <princejason109@gmail.com>
2023-08-14 12:24:29 +0100

    fix for netlify deploy bug

--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -5,18 +5,18 @@ import styled from 'styled-components'
 const AuthWrapper = ({ children }) => {
   const { isLoading, error } = useAuth0()
   
-  if (isLoading) {
-    return <Wrapper>
-      <h1>Loading...</h1>
-    </Wrapper>
-  }
+  // if (isLoading) {
+  //   return <Wrapper>
+  //     <h1>Loading...</h1>
+  //   </Wrapper>
+  // }
 
-  if (error) {
-    return <Wrapper>
-      <h1>{ error.message}</h1>
-    </Wrapper>
-  }
-  return children 
+  // if (error) {
+  //   return <Wrapper>
+  //     <h1>{ error.message}</h1>
+  //   </Wrapper>
+  // }
+  return <>{ children }</>
 }
 
 const Wrapper = styled.section`
